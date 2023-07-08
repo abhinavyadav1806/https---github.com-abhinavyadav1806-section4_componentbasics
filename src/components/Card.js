f646432ed99378e3b4f9be3f7a7b98762ff3a273
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-07-08 19:50:16 +0530

    Static webpage

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+const Card = ( props ) => {
+    return (
+        <div>
+            <ul id="concepts">
+                <li className="concept">
+                    <img src={props.items[0].image} alt="Components" />
+                    <h2>{props.items[0].title}</h2>
+                    <p>{props.items[0].description}</p>
+                </li>
+
+                <li className="concept">
+                    <img src={props.items[1].image} alt="Components" />
+                    <h2>{props.items[1].title}</h2>
+                    <p>{props.items[1].description}</p>
+                </li>
+
+                <li className="concept">
+                    <img src={props.items[2].image} alt="Components" />
+                    <h2>{props.items[2].title}</h2>
+                    <p>{props.items[2].description}</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Card;
